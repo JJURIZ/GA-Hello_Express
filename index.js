8c27b830a0f22a5f87eb83420c12fdc087c8eccd
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-13 12:40:49 -0500

    [index] Added two more routes

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello, SEI World!');
+    res.send(`Hello, SEI World! It's me, Jeremy! Thanks for stopping by. My blog is at localhost:8000/blog. Check it out!`);
+})
+
+app.get('/about', function(req, res) {
+    res.send('You\'ve reached the about page!');
+})
+
+app.get('/blog', (req, res) => {
+    res.send('This is the blog of Jeremy Bearimy');
 })
 
 app.listen(8000);
