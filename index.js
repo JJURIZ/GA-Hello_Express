f496320b40ee7fb1102ce8ff179177b3d36acf1c
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-13 19:43:09 -0500

    [ejs files] installed EJS and converted files

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
-    res.send(`Hello, SEI World! It's me, Jeremy! Thanks for stopping by. My blog is at localhost:8000/blog. Check it out!`);
+    res.render('index')
 })
 
 app.get('/about', function(req, res) {
-    res.send('You\'ve reached the about page!');
+    res.render('about')
 })
 
-app.get('/blog', (req, res) => {
-    res.send('This is the blog of Jeremy Bearimy');
+const blog = app.get('/blog/:date', (req, res) => {
+    const theDateTheyWant = req.params.date
+    res.render('blog', { date: theDateTheyWant })
 })
 
-app.listen(8000);
+app.listen(8000, () => {
+    console.log('server started!')
+});
+
